711e3e5aa96d6d4d291f2a52f965d45f61800b8e
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-08-02 21:33:40 -0500

    feat: finished basic crud methods used postman for requests and testing.

--- restapi/main.go
+++ restapi/main.go
@@ -12,7 +12,7 @@ import (
 // Init Posts var as a slice Post struct
 var posts []Post
 
-// Post Structs
+// Post Structs similar
 type Post struct {
 	ID 			string 	`json:"id"`
 	Title 		string 	`json:"title"`
@@ -55,19 +55,37 @@ func createPost(w http.ResponseWriter, r *http.Request) {
 	post.ID = strconv.Itoa(rand.Intn(1000000)) // mock id
 	posts = append(posts, post)
 	json.NewEncoder(w).Encode(post)
-
-
-
 }
 
-// Update Book
+// Update Post
 func updatePost(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range posts {
+		if item.ID == params["id"] {
+			posts = append(posts[:index], posts[index+1:]...)
+			var post Post
+			_ = json.NewDecoder(r.Body).Decode(&post)
+			post.ID = params["id"]
+			posts = append(posts, post)
+			json.NewEncoder(w).Encode(post)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(posts)
 }
 
-// Delete Book
+// Delete Post
 func deletePost(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range posts {
+		if item.ID == params["id"] {
+			posts = append(posts[:index], posts[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(posts)
 }
 
 func main() {

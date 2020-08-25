8097dd514a6c85b539fcae5fbcd2f7c35280c37a
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-08-24 19:14:13 -0500

    feat: converting type.

--- go-playground/main.go
+++ go-playground/main.go
@@ -16,7 +16,17 @@ func main() {
 	myNumber = 3.14
 	fmt.Println(myNumber)
 
+	var x int = 5
+	x++
+	fmt.Println(x)
 
+	var num int
+	num = 32
+	fmt.Printf("%v, %T\n", num, num)
+
+	var numFloat float32
+	numFloat = float32(x)
+	fmt.Printf("%v, %T\n", numFloat, numFloat)
 
 
 }

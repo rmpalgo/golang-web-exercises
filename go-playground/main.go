4aafd778642c1809d165e5ceebf1695c5aeb41da
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-08-24 19:10:54 -0500

    feat: exercise 1-6 complete.

---
+++ go-playground/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+//refresher exercise
+func main() {
+
+	var myFavoriteNumber int
+	myFavoriteNumber = 4
+	fmt.Println(myFavoriteNumber)
+
+	var myString string = "hello"
+	fmt.Println(myString)
+
+	var myNumber float32
+	myNumber = 3.14
+	fmt.Println(myNumber)
+
+
+
+
+}

e21f65ff774ddae810397dd7b0322915fcec49c2
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-09-02 15:03:53 -0500

    feat: struct with lowercase fields can be seen in package.

--- 12_structs/main.go
+++ 12_structs/main.go
@@ -1,6 +1,20 @@
 package main
 
-//Structs
+import "fmt"
+
+//arrays, slices, maps, have to have same data types whereas struct do not
+
+
+
+//Structs Review
+type Doctor struct {
+	number int
+	actorName string
+	companions []string
+	episodes []string
+}
+
+
 //What are they?
 //Creating
 //CreatingNaming conventions
@@ -9,4 +23,15 @@ package main
 
 
 func main() {
+	//best method is to place field names
+	aDoctor := Doctor{
+		number : 3,
+		actorName: "Jon Pertwee",
+		companions: []string {
+			"Liz Shaw",
+			"Jo Grant",
+			"Sarah Jane Smith",
+		},
+	}
+fmt.Println(aDoctor)
 }

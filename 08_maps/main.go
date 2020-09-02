b7c5bbdfef282c41b05feb91f10994e37320c109
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-09-02 14:18:42 -0500

    feat: overview of map, referencing it will affect original data.

--- 08_maps/main.go
+++ 08_maps/main.go
@@ -3,23 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	////Define map
-	//
-	//emails := make(map[string]string)
-	//
-	//// Assign kv
-	//emails["Ron"] = "ron@gmail.com"
-	//emails["Milo"] = "milo@gmail.com"
-	//emails["Mia"] = "mia@gmail.com"
+	// Maps
+	statePopulations := make(map[string]int)
+	statePopulations = map[string]int{
+		"California":   39250017,
+		"Texas":        27862596,
+		"Florida":      20612439,
+		"New York":     19745289,
+		"Pennsylvania": 12802503,
+		"Illinois":     12801539,
+		"Ohio":         11614373,
+	}
+	//m := map[[3]string]string{}
+	//return of map is not guarantee the order
+	statePopulations["Georgia"] = 10310371
+	//the "ok" will let you know if the key is indeed a key in the map
+	//since non key value will still return 0 as default
+	pop, ok := statePopulations["Ohio"]
+	fmt.Println(pop, ok)
+	fmt.Println(len(statePopulations))
 
-	// Declare map and add kv
-	emails := map[string]string{"Ron":"ron@gmail.com", "Milo":"milo@gmail.com","Mia":"mia@gmail.com"}
+	//when referencing map, will affect original data
+	sp := statePopulations
+	delete(sp, "Ohio")
+	fmt.Println(sp)
+	fmt.Println(statePopulations)
 
-	fmt.Println(emails)
-	fmt.Println(len(emails))
-	fmt.Println(emails["Milo"])
-
-	// Delete from map
-	delete(emails, "Ron")
-	fmt.Println(emails)
 }
